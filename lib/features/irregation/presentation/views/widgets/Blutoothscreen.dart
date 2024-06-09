7c475f1d931f4b96377cf24f34ee8ea193da3fc4
Yasmin Mohamed <yaso.mohamed0987@gmail.com>
2024-06-09 18:09:14 +0300

    update "add irrigation_view_body.dart"

---
+++ lib/features/irregation/presentation/views/widgets/Blutoothscreen.dart
@@ -0,0 +1,179 @@
+import 'package:akhder/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:typed_data';
+import 'dart:convert';
+
+class BluetoothScreen extends StatefulWidget {
+  @override
+  _BluetoothScreenState createState() => _BluetoothScreenState();
+}
+
+class _BluetoothScreenState extends State<BluetoothScreen> {
+  BluetoothConnection? connection;
+  bool isReady = false;
+  String moisturePercentage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    // _initializeBluetooth();
+    connect('00:22:12:02:4B:CC');
+    // _startListening();
+
+  }
+
+  /********************* */
+  void getData(String address) async {
+    try {
+      BluetoothConnection connection =
+      await BluetoothConnection.toAddress(address);
+      print('Connected to the device');
+
+      connection.input!.listen((Uint8List data) {
+        print('Data incoming: ${ascii.decode(data)}');
+        connection.output.add(data); // Sending data
+
+        if (ascii.decode(data).contains('!')) {
+          connection.finish(); // Closing connection
+          print('Disconnecting by local host');
+        }
+      }).onDone(() {
+        print('Disconnected by remote request');
+      });
+    } catch (exception) {
+      print(exception.toString());
+      print('Cannot connect, exception occured');
+    }
+  }
+
+
+// Connect to the selected Arduino device
+  Future<void> _connectToDevice(String address) async {
+    try {
+      connection = await BluetoothConnection.toAddress(address);
+      connection!.input!.listen((Uint8List data) {
+        print('Data incoming: ${ascii.decode(data)}');
+      }).onDone(() {
+        print('Disconnected by remote request');
+      });
+      setState(() {
+        isReady = true;
+      });
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  // Initialize Bluetooth and start scanning for devices
+  Future<void> _initializeBluetooth() async {
+    FlutterBluetoothSerial.instance.state.then((state) {
+      try {
+        if (state == BluetoothState.STATE_ON) {
+          setState(() {
+            isReady = true;
+          });
+          print('connection 1 done');
+        }
+      } catch (e) {
+        print('Error: $e');
+      }
+    });
+
+    try {
+
+      List<BluetoothDiscoveryResult> results = [];
+
+      void startDiscovery() {
+        var streamSubscription = FlutterBluetoothSerial.instance
+            .startDiscovery().listen((r) {
+          results.add(r);
+        });
+
+        streamSubscription.onDone(() {
+          //Do something when the discovery process ends
+        });
+      }
+
+
+
+      List<BluetoothDevice> devices =
+      await FlutterBluetoothSerial.instance.getBondedDevices();
+      print(results.length);
+      print(devices.length);
+
+      await _connectToDevice('00:22:12:02:4B:CC');
+      getData('00:22:12:02:4B:CC');
+      print('connection done');
+
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  // Start listening for Bluetooth data
+
+  void _startListening() async {
+    if (connection == null || connection!.isConnected == false) {
+      print('Bluetooth connection is not established.');
+      return;
+    }
+
+    connection!.input?.listen((Uint8List data) {
+      print('Raw data incoming: $data');
+      try {
+        String decodedData = utf8.decode(data);
+        print('Decoded data: $decodedData');
+        send(data);
+
+        setState(() {
+          moisturePercentage = decodedData;
+        });
+      } catch (e) {
+        print('Error decoding data: $e');
+      }
+    }).onDone(() {
+      print('Bluetooth connection closed');
+    });
+  }
+
+
+  connect(String address) async {
+    try {
+      connection = await BluetoothConnection.toAddress(address);
+      print('Connected to the device');
+      _startListening();
+    } catch (exception) {
+      print('booom boom ${exception.toString()}');
+
+      print('Cannot connect, exception occured');
+    }
+  }
+
+  Future send(Uint8List data) async {
+    connection!.output.add(data);
+    await connection!.output.allSent;
+    print (connection!.output.allSent);
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'الرطوبة',
+         style: Styles.textStyle20.copyWith(
+           color:Colors.white
+         ),
+        ),
+        Text(
+          moisturePercentage.toString(),
+          style:Styles.textStyle20.copyWith(
+    color:Colors.white
+    ),
+        ),
+      ],
+    );
+  }
+}
--- lib/features/irregation/presentation/views/widgets/irrigation_view_body.dart
+++ lib/features/irregation/presentation/views/widgets/irrigation_view_body.dart
@@ -4,79 +4,62 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'Blutoothscreen.dart';
 class IrrigationViewBody extends StatelessWidget {
   const IrrigationViewBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
          Padding(
-          padding: EdgeInsets.only(top: 30.0, right: 350),
-          child: IconButton(onPressed:()
+          padding: EdgeInsets.only(top: 15.0, right: 350),
+          child:
+          IconButton(onPressed:()
           {
             GoRouter.of(context).pop();
           },
-            icon:const Icon(Icons.arrow_back_ios_new_rounded),)
+            icon:const Icon(Icons.arrow_back_ios_new_rounded),),
         ),
-        const SizedBox(
-          height: 135,
-        ),
-        Container(
-          height: 500,
-          width: 500,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(2),
-            image: const DecorationImage(
-              image: AssetImage(AssetsData.irregation),
-              fit: BoxFit.fill,
+        Spacer(),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height:650,
+           width: 650,
+            decoration:const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(AssetsData.irregation),
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 220, top: 10),
-                child: TextButton(
-                  onPressed: () {},
-                  child: Column(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(16),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 220, top: 10),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        const SizedBox(
+                          height: 100,
                         ),
-                        height: 80,
-                        width: 80,
-                        child: const Text(
-                          textAlign: TextAlign.center,
-                          'الحرارة'
-                              '   24 °C',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          height: 80,
+                          width: 80,
+                          child: BluetoothScreen()
                         ),
-                      ),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius: BorderRadius.circular(16),
-                        ),
-                        height: 80,
-                        width: 80,
-                        child: const Text(
-                          textAlign: TextAlign.center,
-                          'الرطوبة'
-                              '   24 %',
-                          style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ],
